868af3af301ea1f7535d3907114ec3fe57413200
Bertolot <lucasbertolot@hotmail.com>
2023-05-10 01:08:43 -0300

    add navbar

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,21 @@
+import { Container, Box, Heading } from "@chakra-ui/react"
+
 const Page = () => {
     return(
-        <div>Hello</div>
+        <Container>
+            <Box borderRadius="lg" bg="red" p={3} mb={6} aling="center">
+                Hello, I&apos;m a full-stack developer based in Brazil!
+            </Box>
+
+            <Box display={{md:'flex'}}>
+                <Box flexGrow={1}>
+                    <Heading as="h2" variant="page-title">
+                        Lucas Bertolot
+                    </Heading>
+                    <p>Full stack Developer</p>        
+                </Box>
+            </Box>
+        </Container>
     )
 }
 
